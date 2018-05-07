acfebe854eb43586464c6134320c5137c39bc2aa
Anthony Lam <al3101@nyu.edu>
2018-05-07 23:03:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,3 +24,14 @@ def word_substituter(tweet)
   end 
   array.join(" ")
 end 
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet| 
+    puts word_substituter(tweet)
+  end 
+end 
+
+
+def selective_tweet_shortener(tweet)
+  
