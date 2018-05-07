2fd0aa23559c1594bba3a7990da3200a9597b12e
Anthony Lam <al3101@nyu.edu>
2018-05-07 22:32:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+dictionary_hash = {"hello" => "hi", 
+  "to" => "2",
+  "two" => "2", 
+  "too" => "2", 
+  "for" => "4",
+  "four" => "4", 
+  "be" => "b", 
+  "you" => "u", 
+  "at" => "@" ,
+  "and" => "&"}
+  
+
+def word_substituter(tweet, dictionary_hash)
+  tweet = tweet.split
+  tweet.collect do |word|
+    if dictionary_hash.keys.include?(word)
+      word = dictionary_hash[word]
+    end
+  end
+  tweet = tweet.join(" ")
+end 
